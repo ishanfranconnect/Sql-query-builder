852689a8b3bcbce363e81e83775afe651fb934a4
Kartik Bhardwaj <Kartik.bhardwaj@franconnect.com>
2026-04-21 16:15:55 +0530

    testing

--- Sql-query-builder/src/main/java/com/Sql_query_builder/SqlQueryBuilderApplication.java
+++ Sql-query-builder/src/main/java/com/Sql_query_builder/SqlQueryBuilderApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SqlQueryBuilderApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SqlQueryBuilderApplication.class, args);
+
+        SpringApplication.run(SqlQueryBuilderApplication.class, args);
+        System.out.println("changes from kartik");
 	}
 
 }
