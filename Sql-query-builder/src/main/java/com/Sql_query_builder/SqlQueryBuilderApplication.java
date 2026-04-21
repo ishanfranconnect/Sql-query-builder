ca9a855b23c0227d6cda05dbc88ce2002907c0ba
Ishan Anand <ishan.anand@franconnect.com>
2026-04-21 14:42:22 +0530

    created the respository from the spring intizer

---
+++ Sql-query-builder/src/main/java/com/Sql_query_builder/SqlQueryBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.Sql_query_builder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlQueryBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlQueryBuilderApplication.class, args);
+	}
+
+}
---
+++ Sql-query-builder/src/test/java/com/Sql_query_builder/SqlQueryBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Sql_query_builder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlQueryBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
